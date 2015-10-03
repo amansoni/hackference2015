faaa499d5c392ee8cbc7f4fd58e326455cac2b1e
Hassan Qulqass <hassan@pseudoh.com>
2015-10-03 17:05:26 +0100

    process.php cors

--- process.php
+++ process.php
@@ -1,6 +1,7 @@
 <?php
 
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
 
 $endpoint = "http://api.majestic.com/api/json?app_api_key=AA3D7FEDA8E34089FB821FF4A2A97EBA&cmd=GetRefDomains&item0=".$_GET['url']."&Count=10000&datasource=fresh";
 
