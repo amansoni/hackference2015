15ceb835a27cbc8fae6c7b681925fd4cf65042df
Aman Soni <sonia2@aston.ac.uk>
2015-10-03 16:30:01 +0100

    2 degrees of separation

--- js/twitter.js
+++ js/twitter.js
@@ -1,18 +1,23 @@
+postlinks = [];
+
 function get1stDegree(){
   console.log('1st degree');
   groupurl = $('#groupurl');
   console.log(groupurl.val());
-  postlinks = [];
   $.get( groupurl.val(), function( data ) {
     links = $(data).find("a[href*='twitter.com']");
     $.each( links, function( key, value ) {
       $('#twitterhandles').val($('#twitterhandles').val()+ value + '\n');
-      postlinks.push(value);
+      console.log(value.href );
+      postlinks.push(value.href);
     });
   });
+}
 
+function get2ndDegree(){
+  console.log('postlinks' + postlinks);
   console.log('2nd degree');
-  url2 = '/process.php';
+  url2 = 'process.php';
   // Send the data using post
   var posting = $.post( url2, { s: postlinks } );
 
