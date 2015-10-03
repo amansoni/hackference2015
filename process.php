616aa6847059faa86d74ebb6781bf520ff2a4900
Hassan Qulqass <hassan@pseudoh.com>
2015-10-03 19:05:20 +0100

    changed count to 1000

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 
-$endpoint = "http://api.majestic.com/api/json?app_api_key=AA3D7FEDA8E34089FB821FF4A2A97EBA&cmd=GetRefDomains&item0=".$_GET['url']."&Count=10000&datasource=fresh";
+$endpoint = "http://api.majestic.com/api/json?app_api_key=AA3D7FEDA8E34089FB821FF4A2A97EBA&cmd=GetRefDomains&item0=".$_GET['url']."&Count=1000&datasource=fresh";
 
 $json = file_get_contents($endpoint);
 echo $json;
