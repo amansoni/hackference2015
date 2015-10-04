02f5d1bba0285469dd1f80b119934b4900ff9e0f
Aman Soni <sonia2@aston.ac.uk>
2015-10-04 10:40:03 +0100

    Added Braintree, 2 degrees for live demo

--- twilio.php
+++ twilio.php
@@ -1,15 +1,24 @@
 <?php
     header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 $file = "twitter.txt";
-$fh = fopen($file, 'a') or die("can't open file");
+
+$myfile = fopen($file, "a") or die("Unable to open file!");
 // get twitter handle from message
+$text = $_REQUEST['Body'];
+$from = $_REQUEST['From'];
 $data = "@amansoni\n";
-fwrite($fh, $data);
-fclose($fh);
+fwrite($myfile, "\n". $data);
+fclose($myfile);
+
+$file = "log.txt";
+$myfile = fopen($file, "a") or die("Unable to open file!");
+$debug = var_export($_POST, true);
+fwrite($myfile, "\n". $debug);
+fclose($myfile);
 
 ?>
+<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Message>Hello, Hackferencer! We've got your twitter as: <?php echo $data ?></Message>
+    <Message>Hello, Hackferencer! We got your twitter as: <?php echo $data ?></Message>
 </Response>
